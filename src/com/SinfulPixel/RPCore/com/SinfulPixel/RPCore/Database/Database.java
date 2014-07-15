5a0b78c1f392a086d1bfaa65720b733f447c1db3
Min3CraftDud3 <hubeb93@gmail.com>
2014-07-15 18:12:27 -0400

    Setting up database connections.

---
+++ src/com/SinfulPixel/RPCore/com/SinfulPixel/RPCore/Database/Database.java
@@ -0,0 +1,21 @@
+package com.SinfulPixel.RPCore.com.SinfulPixel.RPCore.Database;
+
+import com.SinfulPixel.RPCore.RPCore;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public abstract class Database {
+    protected RPCore plugin;
+    protected Database(RPCore plugin) {
+        this.plugin = plugin;
+    }
+    public abstract Connection openConnection() throws SQLException, ClassNotFoundException;
+    public abstract boolean checkConnection() throws SQLException;
+    public abstract Connection getConnection();
+    public abstract boolean closeConnection() throws SQLException;
+    public abstract ResultSet querySQL(String query) throws SQLException, ClassNotFoundException;
+    public abstract int updateSQL(String query) throws SQLException, ClassNotFoundException;
+}
