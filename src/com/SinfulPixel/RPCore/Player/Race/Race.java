f51ef1a44e700271a1676241476c15af7ed1a4da
Min3CraftDud3 <hubeb93@gmail.com>
2014-07-19 15:24:07 -0400

    Starting to implement Race & Skills

---
+++ src/com/SinfulPixel/RPCore/Player/Race/Race.java
@@ -0,0 +1,7 @@
+package com.SinfulPixel.RPCore.Player.Race;
+
+/**
+ * Created by Min3 on 7/19/2014.
+ */
+public abstract class Race {
+}
---
+++ src/com/SinfulPixel/RPCore/Player/Skills/Skills.java
@@ -0,0 +1,8 @@
+package com.SinfulPixel.RPCore.Player.Skills;
+
+/**
+ * Created by Min3 on 7/19/2014.
+ */
+public abstract class Skills {
+
+}
