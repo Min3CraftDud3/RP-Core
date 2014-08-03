7504ec1371f17857de9d3ee3ab156af37b82faa7
Min3CraftDud3 <hubeb93@gmail.com>
2014-08-03 04:09:13 -0400

    bug fixed

--- src/com/SinfulPixel/RPCore/RPCore.java
+++ src/com/SinfulPixel/RPCore/RPCore.java
@@ -23,46 +23,45 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 
-public class RPCore extends JavaPlugin{
-	ConfigMgr cfg = new ConfigMgr(this);
-	Lag lag = new Lag(this);
-	Chat chat = new Chat(this);
-	PListener pl = new PListener(this);
-	Bank bank = new Bank(this);
-	MoneyHandler mh = new MoneyHandler(this);
-	PetMgr pet = new PetMgr(this);
-	CombatMgr cbt = new CombatMgr(this);
-	Bounty mb = new Bounty(this);
-	public EnchantGlow glow = new EnchantGlow(120);
-    ;
+public class RPCore extends JavaPlugin {
+    ConfigMgr cfg = new ConfigMgr(this);
+    Lag lag = new Lag(this);
+    Chat chat = new Chat(this);
+    PListener pl = new PListener(this);
+    Bank bank = new Bank(this);
+    MoneyHandler mh = new MoneyHandler(this);
+    PetMgr pet = new PetMgr(this);
+    CombatMgr cbt = new CombatMgr(this);
+    Bounty mb = new Bounty(this);
+    public EnchantGlow glow = new EnchantGlow(120);
 
     MySQL MySQL = new MySQL(this, getConfig().getString("RPCore.MySQL.Host"), getConfig().getString("RPCore.MySQL.Port"),
-    getConfig().getString("RPCore.MySQL.Database"), getConfig().getString("RPCore.MySQL.Username"), getConfig().getString("RPCore.MySQL.Password"));
+            getConfig().getString("RPCore.MySQL.Database"), getConfig().getString("RPCore.MySQL.Username"), getConfig().getString("RPCore.MySQL.Password"));
     static Connection c = null;
     public static Statement statement = null;
-	
-	
-	public void onEnable(){
-		//Runnables
-		@SuppressWarnings("unused")
-		BukkitTask TaskName = new MoneyUpdater(this).runTaskTimer(this, 60*20, 60*20);
-		//Register Events
-		getServer().getPluginManager().registerEvents(chat, this);
-		getServer().getPluginManager().registerEvents(pl, this);
-		getServer().getPluginManager().registerEvents(mh, this);
-		getServer().getPluginManager().registerEvents(pet, this);
-		getServer().getPluginManager().registerEvents(cbt, this);
-		getServer().getPluginManager().registerEvents(mb, this);
-        getServer().getPluginManager().registerEvents(new Backpack(this),this);
-		//Create Default Config
-		try {
-		      saveConfig();
-		      setupConfig(getConfig());
-		      saveConfig();
-              Backpack.createBPConfig();
-		    } catch (Exception e) {
-		      e.printStackTrace();
-		    }
+
+
+    public void onEnable() {
+        //Runnables
+        @SuppressWarnings("unused")
+        BukkitTask TaskName = new MoneyUpdater(this).runTaskTimer(this, 60 * 20, 60 * 20);
+        //Register Events
+        getServer().getPluginManager().registerEvents(chat, this);
+        getServer().getPluginManager().registerEvents(pl, this);
+        getServer().getPluginManager().registerEvents(mh, this);
+        getServer().getPluginManager().registerEvents(pet, this);
+        getServer().getPluginManager().registerEvents(cbt, this);
+        getServer().getPluginManager().registerEvents(mb, this);
+        getServer().getPluginManager().registerEvents(new Backpack(this), this);
+        //Create Default Config
+        try {
+            saveConfig();
+            setupConfig(getConfig());
+            saveConfig();
+            Backpack.createBPConfig();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         //MySQL
         try {
             if (getConfig().getBoolean("RPCore.MySQL.UseMySQL")) {
@@ -71,8 +70,8 @@ public class RPCore extends JavaPlugin{
                 System.out.println("Connecting to Database...CONNECTED!");
                 statement = c.createStatement();
                 DatabaseMetaData meta = c.getMetaData();
-                ResultSet res = meta.getTables(null,null,"RPCORE",null);
-                if(!res.next()) {
+                ResultSet res = meta.getTables(null, null, "RPCORE", null);
+                if (!res.next()) {
                     System.out.println("Creating Core Table");
                     statement.executeUpdate("CREATE TABLE RPCORE (UUID varchar(38) NOT NULL UNIQUE PRIMARY KEY,PNAME varchar(30) NOT NULL, " +
                             "ACCOUNTBAL DECIMAL(10,2), RACE varchar(20));");
@@ -83,88 +82,97 @@ public class RPCore extends JavaPlugin{
                     c.setAutoCommit(true);
                 }
             }
-        }catch(Exception i){i.printStackTrace();System.out.println("Error Connecting to Database. Please Check your login details.");}
-		//Register Commands
-		getCommand("rpcore").setExecutor(new RPCoreCmd(this));
-		getCommand("diag").setExecutor(new DiagnosticCmd(this));
-		getCommand("cleanup").setExecutor(new CleanupCmd(this));
-		getCommand("local").setExecutor(new ChatCmd(this));
-		getCommand("roleplay").setExecutor(new ChatCmd(this));
-		getCommand("trade").setExecutor(new ChatCmd(this));
-		getCommand("global").setExecutor(new ChatCmd(this));
-		getCommand("admin").setExecutor(new ChatCmd(this));
-		getCommand("money").setExecutor(new EconomyCmd(this));
-		getCommand("pay").setExecutor(new EconomyCmd(this));
-		getCommand("EcoReset").setExecutor(new EconomyCmd(this));
-		getCommand("EcoRemove").setExecutor(new EconomyCmd(this));
-		getCommand("EcoAdd").setExecutor(new EconomyCmd(this));
-		getCommand("Eco").setExecutor(new EconomyCmd(this));
-		getCommand("EcoLookUp").setExecutor(new EconomyCmd(this));
-		getCommand("petme").setExecutor(new PetCmd(this));
-		getCommand("roll").setExecutor(new DiceCmd(this));
-		getCommand("backpack").setExecutor(new BackpackCmd(this));
-		//Register Enchantment
-		try{
-			Field f = Enchantment.class.getDeclaredField("acceptingNew");
-			f.setAccessible(true);
-			f.set(null, true);
-		}catch(Exception e){e.printStackTrace();}
-		try {EnchantmentWrapper.registerEnchantment(glow);
-		}catch(IllegalArgumentException e){}
-	}
-    public void onDisable(){
-       try {
-           Backpack.disable();
-       }catch(IOException i){i.printStackTrace();}
+        } catch (Exception i) {
+            i.printStackTrace();
+            System.out.println("Error Connecting to Database. Please Check your login details.");
+        }
+        //Register Commands
+        getCommand("rpcore").setExecutor(new RPCoreCmd(this));
+        getCommand("diag").setExecutor(new DiagnosticCmd(this));
+        getCommand("cleanup").setExecutor(new CleanupCmd(this));
+        getCommand("local").setExecutor(new ChatCmd(this));
+        getCommand("roleplay").setExecutor(new ChatCmd(this));
+        getCommand("trade").setExecutor(new ChatCmd(this));
+        getCommand("global").setExecutor(new ChatCmd(this));
+        getCommand("admin").setExecutor(new ChatCmd(this));
+        getCommand("money").setExecutor(new EconomyCmd(this));
+        getCommand("pay").setExecutor(new EconomyCmd(this));
+        getCommand("EcoReset").setExecutor(new EconomyCmd(this));
+        getCommand("EcoRemove").setExecutor(new EconomyCmd(this));
+        getCommand("EcoAdd").setExecutor(new EconomyCmd(this));
+        getCommand("Eco").setExecutor(new EconomyCmd(this));
+        getCommand("EcoLookUp").setExecutor(new EconomyCmd(this));
+        getCommand("petme").setExecutor(new PetCmd(this));
+        getCommand("roll").setExecutor(new DiceCmd(this));
+        getCommand("backpack").setExecutor(new BackpackCmd(this));
+        //Register Enchantment
+        try {
+            Field f = Enchantment.class.getDeclaredField("acceptingNew");
+            f.setAccessible(true);
+            f.set(null, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            EnchantmentWrapper.registerEnchantment(glow);
+        } catch (IllegalArgumentException e) {
+        }
     }
-	private void setupConfig(FileConfiguration config) throws IOException{
-	    if (!new File(getDataFolder(), "RESET.FILE").exists()) {
-	      new File(getDataFolder(), "RESET.FILE").createNewFile();
-	      config.set("RPCore.Creator","Min3CraftDud3");
-	      config.set("RPCore.WebSite","http://www.SinfulPixel.com");
-          config.set("RPCore.MySQL.Warning", "====== MySQL Settings ======");
-          config.set("RPCore.MySQL.UseMySQL", false);
-          config.set("RPCore.MySQL.Host", "127.0.0.1");
-          config.set("RPCore.MySQL.Port", 3306);
-          config.set("RPCore.MySQL.Database", "minecraft");
-          config.set("RPCore.MySQL.Username", "UserNameHere");
-          config.set("RPCore.MySQL.Password", "PassHere");
-          config.set("RPCore.General.Warning","====== General Settings ======");
-          config.set("RPCore.General.MaxCombatLogs", 5);
-	      config.set("RPCore.Eco.Warning","====== Economy Settings ======");
-	      config.set("RPCore.Eco.CurrencyName","Coins");
-	      config.set("RPCore.Eco.CurrencyName_Multiple","Coins");
-	      config.set("RPCore.Eco.StartingAmount","10.0");
-	      config.set("RPCore.MobBalance.Warning","====== Mob Bounty Settings ======");
-	      config.set("RPCore.MobBalance.SKELETON","5");
-	      config.set("RPCore.MobBalance.CREEPER","5");
-	      config.set("RPCore.MobBalance.SPIDER","5");
-	      config.set("RPCore.MobBalance.CAVE_SPIDER","5");
-	      config.set("RPCore.MobBalance.ZOMBIE","5");
-	      config.set("RPCore.MobBalance.VILLAGER","5");
-	      config.set("RPCore.MobBalance.ENDER_DRAGON","5");
-	      config.set("RPCore.MobBalance.WITCH","5");
-	      config.set("RPCore.MobBalance.COW","5");
-	      config.set("RPCore.MobBalance.BAT","5");
-	      config.set("RPCore.MobBalance.CHICKEN","5");
-	      config.set("RPCore.MobBalance.PIG","5");
-	      config.set("RPCore.MobBalance.SQUID","5");
-	      config.set("RPCore.MobBalance.IRON_GOLEM","5");
-	      config.set("RPCore.MobBalance.SNOWMAN","5");
-	      config.set("RPCore.MobBalance.SHEEP","5");
-	      config.set("RPCore.MobBalance.SILVERFISH","5");
-	      config.set("RPCore.MobBalance.MAGMA_CUBE","5");
-	      config.set("RPCore.MobBalance.SLIME","5");
-	      config.set("RPCore.MobBalance.GHAST","5");
-	      config.set("RPCore.MobBalance.PIG_ZOMBIE","5");
-	      config.set("RPCore.MobBalance.WOLF","5");
-	      config.set("RPCore.MobBalance.MUSHROOM_COW","5");
-	      config.set("RPCore.MobBalance.BLAZE","5");
-	      config.set("RPCore.MobBalance.OCELOT","5");
-	      config.set("RPCore.MobBalance.WITHER","5");
-	      config.set("RPCore.MobBalance.GIANT","5");
-	      config.set("RPCore.MobBalance.PLAYER","0");
-	      saveConfig();
-	     }
-	  }
+    public void onDisable() {
+        try {
+            Backpack.disable();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+    private void setupConfig(FileConfiguration config) throws IOException {
+        if (!new File(getDataFolder(), "RESET.FILE").exists()) {
+            new File(getDataFolder(), "RESET.FILE").createNewFile();
+            config.set("RPCore.Creator", "Min3CraftDud3");
+            config.set("RPCore.WebSite", "http://www.SinfulPixel.com");
+            config.set("RPCore.MySQL.Warning", "====== MySQL Settings ======");
+            config.set("RPCore.MySQL.UseMySQL", false);
+            config.set("RPCore.MySQL.Host", "127.0.0.1");
+            config.set("RPCore.MySQL.Port", 3306);
+            config.set("RPCore.MySQL.Database", "minecraft");
+            config.set("RPCore.MySQL.Username", "UserNameHere");
+            config.set("RPCore.MySQL.Password", "PassHere");
+            config.set("RPCore.General.Warning", "====== General Settings ======");
+            config.set("RPCore.General.MaxCombatLogs", 5);
+            config.set("RPCore.Eco.Warning", "====== Economy Settings ======");
+            config.set("RPCore.Eco.CurrencyName", "Coins");
+            config.set("RPCore.Eco.CurrencyName_Multiple", "Coins");
+            config.set("RPCore.Eco.StartingAmount", "10.0");
+            config.set("RPCore.MobBalance.Warning", "====== Mob Bounty Settings ======");
+            config.set("RPCore.MobBalance.SKELETON", "5");
+            config.set("RPCore.MobBalance.CREEPER", "5");
+            config.set("RPCore.MobBalance.SPIDER", "5");
+            config.set("RPCore.MobBalance.CAVE_SPIDER", "5");
+            config.set("RPCore.MobBalance.ZOMBIE", "5");
+            config.set("RPCore.MobBalance.VILLAGER", "5");
+            config.set("RPCore.MobBalance.ENDER_DRAGON", "5");
+            config.set("RPCore.MobBalance.WITCH", "5");
+            config.set("RPCore.MobBalance.COW", "5");
+            config.set("RPCore.MobBalance.BAT", "5");
+            config.set("RPCore.MobBalance.CHICKEN", "5");
+            config.set("RPCore.MobBalance.PIG", "5");
+            config.set("RPCore.MobBalance.SQUID", "5");
+            config.set("RPCore.MobBalance.IRON_GOLEM", "5");
+            config.set("RPCore.MobBalance.SNOWMAN", "5");
+            config.set("RPCore.MobBalance.SHEEP", "5");
+            config.set("RPCore.MobBalance.SILVERFISH", "5");
+            config.set("RPCore.MobBalance.MAGMA_CUBE", "5");
+            config.set("RPCore.MobBalance.SLIME", "5");
+            config.set("RPCore.MobBalance.GHAST", "5");
+            config.set("RPCore.MobBalance.PIG_ZOMBIE", "5");
+            config.set("RPCore.MobBalance.WOLF", "5");
+            config.set("RPCore.MobBalance.MUSHROOM_COW", "5");
+            config.set("RPCore.MobBalance.BLAZE", "5");
+            config.set("RPCore.MobBalance.OCELOT", "5");
+            config.set("RPCore.MobBalance.WITHER", "5");
+            config.set("RPCore.MobBalance.GIANT", "5");
+            config.set("RPCore.MobBalance.PLAYER", "0");
+            saveConfig();
+        }
+    }
 }
