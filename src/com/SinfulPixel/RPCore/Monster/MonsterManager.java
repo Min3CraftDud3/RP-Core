9b7a16f9db74abce5bfd618de814599884314608
Nick Stanford <lantra271@gmail.com>
2014-09-25 17:31:26 -0400

    epicenter based test 1

--- src/com/SinfulPixel/RPCore/Monster/MonsterManager.java
+++ src/com/SinfulPixel/RPCore/Monster/MonsterManager.java
@@ -1,6 +1,8 @@
 package com.SinfulPixel.RPCore.Monster;
 
 import com.SinfulPixel.RPCore.RPCore;
+import org.bukkit.World;
+import org.bukkit.Chunk;
 import org.bukkit.craftbukkit.v1_7_R3.entity.CraftEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,17 +16,43 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 public class MonsterManager implements Listener
 {
     RPCore plugin;
-    int level;
+    World world;
 
-    public MonsterManager (RPCore plugin)
+    public MonsterManager (RPCore plugin, World world)
 
     {
       this.plugin = plugin;
-      level = 5;
+      this.world = world;
     }
 
     @EventHandler
     public void onCreatureSpawn (CreatureSpawnEvent e)
+    {
+        int level;
+        //determine the nearest epicenter
+
+
+        //determine level by calculating the distance to the nearest epicenter
+        Chunk mstrChunk = e.getLocation().getChunk();
+        Chunk epicenterChunk = world.getChunkAt(world.getSpawnLocation());
+        double mCX = mstrChunk.getX();
+        double mCZ = mstrChunk.getZ();
+        double eCX = epicenterChunk.getX();
+        double eCZ = epicenterChunk.getZ();
+
+        // distance formula
+        int distance = (int) Math.sqrt(Math.pow((mCX - eCX), 2) + Math.pow((mCZ - eCZ),2));
+
+        if (distance <= 1 ) level = 1;
+        else if (distance <= 250) level = distance /2; //every two chunks from the epicenter is a new level
+        else level = 125;
+
+
+
+        customMonsterSpawn(e, level);                               //set the level of the creature
+    }
+
+    public void customMonsterSpawn (CreatureSpawnEvent e, int level)
     {
         if (((CraftEntity) e.getEntity()).getHandle() instanceof CustomEntitySkeleton) {
             ((CustomEntitySkeleton) ((CraftEntity) e.getEntity()).getHandle()).setLevel(level);
