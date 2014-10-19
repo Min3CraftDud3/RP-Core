e1e47bd3b094f34084a851bd0f1fad7ada405565
Min3CraftDud3 <hubeb93@gmail.com>
2014-10-18 21:50:11 -0400

    Reworking Permissions

---
+++ src/main/java/com/SinfulPixel/RPCore/Permissions/PermCfg.java
@@ -0,0 +1,40 @@
+package com.SinfulPixel.RPCore.Permissions;
+
+import com.SinfulPixel.RPCore.RPCore;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Created by Min3 on 10/18/2014.
+ */
+public class PermCfg {
+    static RPCore plugin;
+    public PermCfg(RPCore pl){this.plugin=pl;}
+
+    public static void createConfig(){
+        System.out.println("Creating Permission File...");
+        try {
+            File permDir = new File(plugin.getDataFolder() + File.separator + "Permissions");
+            if (!permDir.exists()) {
+                permDir.mkdir();
+            }
+            File permFile = new File(plugin.getDataFolder() + File.separator + "Permissions" + File.separator + "permissions.yml");
+            if (!permFile.exists()) {
+                permFile.createNewFile();
+                FileConfiguration fc = YamlConfiguration.loadConfiguration(permFile);
+                fc.set("Permissions.Header", "======== [ Permissions File ] ========");
+                fc.set("Permissions.Group.TestPerm.Default", true);
+                fc.set("Permissions.Group.TestPerm.Prefix", "[Member]");
+                fc.set("Permissions.Group.TestPerm.Suffix", "&f");
+                fc.set("Permissions.Group.TestPerm.Permissions", Arrays.asList("permission.perm1","-permission.perm2","permission.perm3"));
+                fc.save(permFile);
+            }
+            System.out.println("Creating Permission File...COMPLETE!");
+        }catch(IOException e){e.printStackTrace();}
+    }
+
+}
